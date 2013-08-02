70526af647dacd5cb50b0e8f0f74450e411c8cc6
Mahsa Lotfi <mahsa.lotfi92@gmail.com>
2013-08-02 18:43:49 +0100

    budget bar shows

--- budget/views.py
+++ budget/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from django.shortcuts import render
 from cat.models import Cat
+from budget.models import Bug
 def budgetForm (req):
-    return render(req, 'budget.html', {'cats': Cat.objects.filter(isSub=False)})
+    return render(req, 'budget.html', {'cats': Cat.objects.filter(isSub=False)}, {'bugs': Bug.objects.all()})
