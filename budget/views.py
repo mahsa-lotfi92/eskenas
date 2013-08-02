c8aac67006fcf55d5edbe2a7b9550835133774c5
Mahsa Lotfi <mahsa.lotfi92@gmail.com>
2013-08-02 20:20:16 +0100

    debug select

--- budget/views.py
+++ budget/views.py
@@ -5,6 +5,6 @@ from budget.models import Bug
 def budgetForm (req):
     if req.method == "POST":
         if req.POST['formID']=="1":
-            new = Bug(limit = req.POST['limit'], Cat.objects.get(req.POST['Category']))
+            new = Bug(limit = req.POST['limit'],bugCat= Cat.objects.get(id=req.POST['catList']))
             new.save()
     return render(req, 'budget.html', {'cats': Cat.objects.filter(isSub=False), 'bugs': Bug.objects.all()})
