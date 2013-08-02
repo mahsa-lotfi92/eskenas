b8c1084a9533ec5add438b438e9c8f9489d72bca
Mahsa Lotfi <mahsa.lotfi92@gmail.com>
2013-08-02 19:58:40 +0100

    budget add debug

--- budget/views.py
+++ budget/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 from cat.models import Cat
 from budget.models import Bug
 def budgetForm (req):
+    if req.method == "POST":
+        if req.POST['formID']=="1":
+            new = Bug(limit = req.POST['limit'], Cat.objects.get(req.POST['Category']))
+            new.save()
     return render(req, 'budget.html', {'cats': Cat.objects.filter(isSub=False), 'bugs': Bug.objects.all()})
