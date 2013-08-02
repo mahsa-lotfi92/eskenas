df7cc184c3f9aca00ea2e6c42e6a4aaae93e39d6
Mahsa Lotfi <mahsa.lotfi92@gmail.com>
2013-08-02 18:16:13 +0100

    cat in budget

--- budget/views.py
+++ budget/views.py
@@ -1,4 +1,5 @@
 # Create your views here.
 from django.shortcuts import render
+from cat.models import Cat
 def budgetForm (req):
-    return render(req, 'budget.html')
+    return render(req, 'budget.html', {'cats': Cat.objects.filter(isSub=False)})
