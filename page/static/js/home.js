5f86aaa915abff816d90c2ad2d454409b214ccbe
Mahya Maftouni <mahya.maftouni@gmail.com>
2013-08-11 13:29:38 +0430

    :D

--- page/static/js/home.js
+++ page/static/js/home.js
@@ -1,15 +1,20 @@
 $(function() {
-		$('#login_link').click(function(ev) {
-			ev.preventDefault();
+	$('#login_link').click(function(ev) {
+		ev.preventDefault();
 		$('#login').css('display', 'inline');
 	});
-		$('#reg_link').click(function(ev) {
-			ev.preventDefault();
+	$('#reg_link').click(function(ev) {
+		ev.preventDefault();
 		$('#reg').css('display', 'inline');
 	});
-		$('.closePic').click(function(ev) {
-			ev.preventDefault();
+	$('.closePic').click(function(ev) {
+		ev.preventDefault();
 		$(this).parent().parent().css('display', 'none');
 	});
-	
+	$('.menuItem').click(function(ev) {
+		ev.preventDefault();
+		$('.menuItem').removeClass('active')
+		$(this).addClass('active')
+	})
 });
+
