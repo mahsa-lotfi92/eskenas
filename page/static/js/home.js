77b9a9a21c07e74a8fe18b8d90504a8f33d3c1bb
Mahsa Lotfi <mahsa.lotfi92@gmail.com>
2013-08-11 11:39:13 +0100

    login and register panel revised

--- page/static/js/home.js
+++ page/static/js/home.js
@@ -2,14 +2,17 @@ $(function() {
 	$('#login_link').click(function(ev) {
 		ev.preventDefault();
 		$('#login').css('display', 'inline');
+		$('#main').css('opacity', '0.5');
 	});
 	$('#reg_link').click(function(ev) {
 		ev.preventDefault();
 		$('#reg').css('display', 'inline');
+		$('#main').css('opacity', '0.5');		
 	});
 	$('.closePic').click(function(ev) {
 		ev.preventDefault();
 		$(this).parent().parent().css('display', 'none');
+		$('#main').css('opacity', '1');
 	});
 	$('.menuItem').click(function(ev) {
 		ev.preventDefault();
