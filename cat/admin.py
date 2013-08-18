aed4da8d95eecb02b6f6d384abc74b30fda315ab
Arman Ordookhani <arman.ordookhani@gmail.com>
2013-08-18 09:47:25 +0100

    admin for bank account

--- cat/admin.py
+++ cat/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from cat.models import Cat
+from cat.models import Cat, BankAccount
 from transaction.models import Transaction
 from budget.models import Bug
 from myprofile.models import userCredit
@@ -10,4 +10,4 @@ admin.site.register(Transaction)
 admin.site.register(Bug)
 admin.site.register(userCredit)
 admin.site.register(user_plan)
-
+admin.site.register(BankAccount)
