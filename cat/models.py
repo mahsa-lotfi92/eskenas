21922a4d83408ae25bdcf4eafcb32b27ec145cff
Arman Ordookhani <arman.ordookhani@gmail.com>
2013-08-18 08:26:00 +0100

    bank account

--- cat/models.py
+++ cat/models.py
@@ -4,6 +4,14 @@ from django.db import models
 class Cat(models.Model):
     name = models.CharField(max_length=50)
     isSub = models.BooleanField(default = False)
-    parentCat= models.ForeignKey('self', null=True, blank=True)
+    parentCat= models.ForeignKey('self', default = None, null=True, blank=True)
+
     def __unicode__ (self):
-        return self.name;
+        return self.name
+
+class BankAccount(models.Model):
+    user = models.ForeignKey('User')
+    name = models.CharField(100)
+
+    def __unicode__(self):
+        return self.name
