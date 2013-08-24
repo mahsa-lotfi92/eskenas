cf967cc07952ec3ebf732142901c702bbecb0aa8
Arman Ordookhani <arman.ordookhani@gmail.com>
2013-08-24 08:19:04 +0100

    clean

--- budget/views.py
+++ budget/views.py
@@ -1,36 +1,33 @@
-# Create your views here.
 from django.shortcuts import render, redirect
 from cat.models import Cat
 from budget.models import Bug
 from transaction.models import Transaction
-from django.db.models.aggregates import Sum
 from myprofile.models import userCredit
 
 
-
-def budgetForm (req):
+def budgetForm(req):
     bugs = Bug.objects.all()
     for i in bugs:
         i.cost = 0
-        for j in Transaction.objects.filter(Category__parentCat=i.bugCat , isIncome=False):
+        for j in Transaction.objects.filter(Category__parentCat=i.bugCat, isIncome=False):
             i.cost += j.cost
-        i.per= i.cost * 100.0 / i.limit
+        i.per = i.cost * 100.0 / i.limit
         
-    return render(req, 'budget.html', {'cats': Cat.objects.filter(isSub=False, user= req.user), 'bugs': bugs, 'myUser': userCredit.objects.get(user= req.user)})
+    return render(req, 'budget.html', {'cats': Cat.objects.filter(isSub=False, user=req.user), 'bugs': bugs, 'myUser': userCredit.objects.get(user=req.user)})
 
 
-def budgetAdd (req):
+def budgetAdd(req):
     new = Bug(limit=req.POST['limit'], bugCat=Cat.objects.get(id=req.POST['catList']))
     new.save()
     return redirect('/budget/') 
 
 
-def budgetDel (req):
+def budgetDel(req):
     Bug.objects.filter(id=req.POST['id']).delete()
     return redirect('/budget/')
 
 
-def budgetEdit (req):
+def budgetEdit(req):
     p = Bug.objects.get(id=req.POST['id'])
     p.limit = req.POST['new']
     p.save()
