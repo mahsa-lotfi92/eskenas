dce3fec2c81889ba11462c33c66d1527c783cb5d
Mahya Maftouni <mahya.maftouni@gmail.com>
2013-08-27 15:38:13 +0430

    budget calendar

--- page/static/js/budget.js
+++ page/static/js/budget.js
@@ -1,18 +1,54 @@
 $(function() {
 	$('#addBudget .btn.btn-success').click(function(ev) {
 		var row = $(this).parent()
-		s= $('#budgetLimit').val()
-		var k=0
-		for (var x=0; x< s.length; x++){
+		s = $('#budgetLimit').val()
+		var k = 0
+		for (var x = 0; x < s.length; x++) {
 			if (s[x] >= '0' && s[x] <= '9') {
-					k++;
+				k++;
 			}
 		}
-		if (k != s.length || s.length==0){
-		ev.preventDefault()
-		$('.budgWar').hide()
-		$('.budgWar').eq(0).show()
+		if($("#budgetLimit").val()==""){
+				ev.preventDefault()
+			$('.budgWar').hide()
+			$('.budgWar').text("مبلغ را وارد کنید.")
+			$('.budgWar').eq(0).show()
+			return false
+			
 		}
+		if (k != s.length || s.length == 0) {
+			ev.preventDefault()
+			$('.budgWar').hide()
+			$('.budgWar').text("مبلغ را به عدد وارد کنید.")
+			$('.budgWar').eq(0).show()
+			return false
+
+		}
+		if ($("#startDate").val() == "") {
+			ev.preventDefault()
+			$('.budgWar').hide()
+			$('.budgWar').text("تاریخ شروع را مشخص کنید.")
+			$('.budgWar').eq(0).show()
+			return false
+
+		}
+		if ($("#endDate").val() == "") {
+			ev.preventDefault()
+			$('.budgWar').hide()
+			$('.budgWar').text("تاریخ پایان را مشخص کنید.")
+			$('.budgWar').eq(0).show()
+			return false
+
+		}
+		if ($("#ctgrID").val() == "") {
+			ev.preventDefault()
+			$('.budgWar').hide()
+			$('.budgWar').text("دسته را مشخص کنید.")
+			$('.budgWar').eq(0).show()
+			return false
+
+		}
+
 	})
 	$('.remainBug').on('click', '.remove', function() {
 		var row = $(this).parent()
@@ -25,36 +61,92 @@ $(function() {
 		row.find('form.edt').removeClass('edt')
 		row.find('#pay').focus()
 	})
-	
+
 	$('.edtSubmit').click(function(ev) {
 		var row = $(this).parent()
-		var i= $(".edtSubmit").index(this);
-		s= $('#pay').eq(i).val()
+		var i = $(".edtSubmit").index(this);
+		s = $('#pay').eq(i).val()
 		console.log(s);
-		var k=0
-		for (var x=0; x< s.length; x++){
+		var k = 0
+		for (var x = 0; x < s.length; x++) {
 			if (s[x] >= '0' && s[x] <= '9') {
-					k++;
+				k++;
 			}
 		}
-		if (k != s.length || s.length==0){
-		ev.preventDefault()
-		$('.budgWar').hide()
-		$('.budgWar').eq(i+1).show();
+		if (k != s.length || s.length == 0) {
+			ev.preventDefault()
+			$('.budgWar').hide()
+			$('.budgWar').eq(i + 1).show();
 		}
 	})
-	
+
 	$('.cancelEdit').click(function(ev) {
 		ev.preventDefault();
 		$('form.forSub').addClass('edt')
 	})
-	
 
-		$(".ctgr").click(function(ev) {
+	$(".ctgr").click(function(ev) {
 		ev.preventDefault();
 		var cid = $(this);
 		$("#ctgrID").val(cid.val());
 		$("#one_level").text(cid.text());
 
+	});
+	$("#startDate").focus(function() {
+
+		new JsDatePick({
+			useMode : 2,
+			target : "startDate",
+			dateFormat : "%Y-%m-%d"
+			/*selectedDate:{				This is an example of what the full configuration offers.
+			 day:5,						For full documentation about these settings please see the full version of the code.
+			 month:9,
+			 year:2006
+			 },
+			 yearsRange:[1978,2020],
+			 limitToToday:false,
+			 cellColorScheme:"beige",
+			 dateFormat:"%m-%d-%Y",
+			 imgPath:"img/",
+			 weekStartDay:5*/
+		});
+	});
+	$("#endDate").focus(function() {
+
+		new JsDatePick({
+			useMode : 2,
+			target : "endDate",
+			dateFormat : "%Y-%m-%d"
+			/*selectedDate:{				This is an example of what the full configuration offers.
+			 day:5,						For full documentation about these settings please see the full version of the code.
+			 month:9,
+			 year:2006
+			 },
+			 yearsRange:[1978,2020],
+			 limitToToday:false,
+			 cellColorScheme:"beige",
+			 dateFormat:"%m-%d-%Y",
+			 imgPath:"img/",
+			 weekStartDay:5*/
+		});
+	});
+	$(".dateEdit").focus(function() {
+
+		new JsDatePick({
+			useMode : 2,
+			target : this.id,
+			dateFormat : "%Y-%m-%d"
+			/*selectedDate:{				This is an example of what the full configuration offers.
+			 day:5,						For full documentation about these settings please see the full version of the code.
+			 month:9,
+			 year:2006
+			 },
+			 yearsRange:[1978,2020],
+			 limitToToday:false,
+			 cellColorScheme:"beige",
+			 dateFormat:"%m-%d-%Y",
+			 imgPath:"img/",
+			 weekStartDay:5*/
+		});
 	});
 })
